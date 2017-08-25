9ca53a1b4a84dc97410ce1a999c6de6d8d22e03d
AndriyMiha <mykhas@gmail.com>
2017-08-25 05:15:42 -0700

    Update Navigation.rb

--- Navigation.rb
+++ Navigation.rb
@@ -1,12 +1,10 @@
 require_relative 'TestSetup.rb'
 
 class PizzaOrder
-    page = SiteSetup.new('http://pizzalviv.com/')
-    sleep 0.2
-    page.close_popup.click  
+    page = SiteSetup.new
+    page.close_popup 
     page.locate_pizza.click
     page.select_pizza.click
-    sleep 1.0
     page.gotobasket.click
     page.verifyammount
     page.checkout_link
