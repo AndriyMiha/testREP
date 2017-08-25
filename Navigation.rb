25df61aa731178e23eccea2fa3575013543c9e49
AndriyMiha <mykhas@gmail.com>
2017-08-25 06:10:37 -0700

    Update Navigation.rb

--- Navigation.rb
+++ Navigation.rb
@@ -3,9 +3,9 @@ require_relative 'TestSetup.rb'
 class PizzaOrder
     page = SiteSetup.new
     page.close_popup 
-    page.locate_pizza.click
-    page.select_pizza.click
-    page.gotobasket.click
+    page.locate_pizza
+    page.select_pizza
+    page.gotobasket
     page.verifyammount
     page.checkout_link
     page.verify_error
